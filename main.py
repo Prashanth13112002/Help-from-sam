0fd3edc1f6d6e10cf65e89eed981db7234d5fabd
ADMIN <prashanthkumaran1302@gmail.com>
2023-07-19 23:52:52 +0530

    mmm

---
+++ main.py
@@ -0,0 +1,11 @@
+def helf_sam(a):
+    count ,i=1,1
+    while (i<a):
+        i*=2
+    if(a==i):
+        return count
+    if(i>a):
+        i/=2
+        b=a-int(i)
+    return count+b
+print(helf_sam(int(input())))
